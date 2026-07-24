08437b23c404540172b6e69b5675a416b913e996
Markus Nieminen <markusnieminen@yahoo.com>
2026-07-24 18:54:44 +0300

    use stations instead of strings of names

--- algorithm/test_algo.go
+++ algorithm/test_algo.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Depth-First-Search
-func FindPathDFS(start, end *data.Station, path *[]string, shortest_route_len *int, best_route *[]string, found_routes *[][]string) {
+func FindPathDFS(start, end *data.Station, path *[]data.Station, shortest_route_len *int, best_route *[]data.Station, found_routes *[][]data.Station) {
 
 	// Ignore loops
 	if start.Visited || len(*path) >= *shortest_route_len {
@@ -15,21 +15,21 @@ func FindPathDFS(start, end *data.Station, path *[]string, shortest_route_len *i
 
 	start.Visited = true
 	data.Events = append(data.Events, data.SearchEvent{Station_Id: start.ID, Visited: true})
-	*path = append(*path, start.Name)
+	*path = append(*path, *start)
 
 	// Check if the node we are looking is in the next nodes
 	if slices.Contains(start.Connections, end) {
 
-		*path = append(*path, end.Name) // Add end station for the prints etc
+		*path = append(*path, *end) // Add end station for the prints etc
 
 		// Should you update the path
 		if len(*path) < *shortest_route_len {
-			*best_route = make([]string, len(*path))
+			*best_route = make([]data.Station, len(*path))
 			copy(*best_route, *path) // CREATE COPY OF THE ITEM, THE PATH WILL CHANGE SO HAVING SAME POINTER WILL CAUSE THE DATA TO BE LOST
 			*shortest_route_len = len(*best_route)
 		}
 
-		copy_of_route := make([]string, len(*path))
+		copy_of_route := make([]data.Station, len(*path))
 		copy(copy_of_route, *path)
 
 		*found_routes = append(*found_routes, copy_of_route)
