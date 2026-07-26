ee8662250424056e6de9f8b77d6e672c042931d8
markusnieminen1 <65071702+markusnieminen1@users.noreply.github.com>
2026-07-26 21:57:21 +0300

    Fiksattu tiedosto

--- algorithm/MaxPaths.go
+++ algorithm/MaxPaths.go
@@ -1,69 +1,69 @@
 package algorithm
 
-import "pathfinder/data"
-import "fmt"
-import "slices"
-import "cmp"
-
-
+import (
+	"cmp"
+	"fmt"
+	"pathfinder/data"
+	"slices"
+)
 
 func MaxPaths(found_routes *[][]string, firstpath *[]string, secondpath *[]string) {
 
-slices.SortFunc((*found_routes), func(a, b []string) int {
-	return cmp.Compare(len(a), len(b))
-})
+	slices.SortFunc((*found_routes), func(a, b []string) int {
+		return cmp.Compare(len(a), len(b))
+	})
 
 	//dereferencing a pointer with ()
-starts := len(data.StationsMap[(*found_routes)[0][0]].Connections)
-ends := len(data.StationsMap[(*found_routes)[0][(len((*found_routes)[0])-1)]].Connections)
-MaxAmountOfPaths := min(ends, starts)
+	starts := len(data.StationsMap[(*found_routes)[0][0]].Connections)
+	ends := len(data.StationsMap[(*found_routes)[0][(len((*found_routes)[0])-1)]].Connections)
+	MaxAmountOfPaths := min(ends, starts)
 
 	//shortest one path "group" always happens, assuming the program stops before if it is invalid, using it to initialize struct
-PathGroup := data.GroupedPaths{Paths: [][]string{}, AmountOfPaths: 0, LongestPathInTurns: 0}
-PathGroup = AddPathToGroup(PathGroup, (*found_routes)[0])
-fmt.Println("Baseline Shortest Single Path ", PathGroup)
+	PathGroup := data.GroupedPaths{Paths: [][]string{}, AmountOfPaths: 0, LongestPathInTurns: 0}
+	PathGroup = AddPathToGroup(PathGroup, (*found_routes)[0])
+	fmt.Println("Baseline Shortest Single Path ", PathGroup)
 
-TopGroupOfGroupedPaths := data.GroupsOfPaths{Groups: []data.GroupedPaths{}}
-TopGroupOfGroupedPaths.Groups = append(TopGroupOfGroupedPaths.Groups, PathGroup)
+	TopGroupOfGroupedPaths := data.GroupsOfPaths{Groups: []data.GroupedPaths{}}
+	TopGroupOfGroupedPaths.Groups = append(TopGroupOfGroupedPaths.Groups, PathGroup)
 
-fmt.Println(MaxAmountOfPaths, "max amount of paths")
+	fmt.Println(MaxAmountOfPaths, "max amount of paths")
 
-if MaxAmountOfPaths > 1{
+	if MaxAmountOfPaths > 1 {
 		// find two and more paths
-FindPathCombo(MaxAmountOfPaths, found_routes, &TopGroupOfGroupedPaths)
-}
+		FindPathCombo(MaxAmountOfPaths, found_routes, &TopGroupOfGroupedPaths)
+	}
 
-fmt.Println(MaxAmountOfPaths, "max vs valid amount found", len(TopGroupOfGroupedPaths.Groups))
-		
-fmt.Println("this is the TOP pathgroup ",TopGroupOfGroupedPaths)
+	fmt.Println(MaxAmountOfPaths, "max vs valid amount found", len(TopGroupOfGroupedPaths.Groups))
+
+	fmt.Println("this is the TOP pathgroup ", TopGroupOfGroupedPaths)
 
 }
 
 func FindPathCombo(MaximumPaths int, found_routes *[][]string, TopGroup *data.GroupsOfPaths) {
 
-PathGroupCombo := data.GroupedPaths{Paths: [][]string{}, AmountOfPaths: 0, LongestPathInTurns: 0}
+	PathGroupCombo := data.GroupedPaths{Paths: [][]string{}, AmountOfPaths: 0, LongestPathInTurns: 0}
 
-OverlapBool := false
+	OverlapBool := false
 
-for _, v := range (*found_routes) {
+	for _, v := range *found_routes {
 
-	fmt.Println(v)
+		fmt.Println(v)
 
-	OverlapBool = false
-	PathCandidate1 := v
-	PathCandidate2 := v
+		OverlapBool = false
+		PathCandidate1 := v
+		PathCandidate2 := v
 
-		for _, w := range (*found_routes){
+		for _, w := range *found_routes {
 
 			PathCandidate2 = w
 
 			fmt.Println(w)
 
-			for _, x := range v[1:len(v)-1]{ 	//choose paths to compare against each other
+			for _, x := range v[1 : len(v)-1] { //choose paths to compare against each other
 
-				for _, y := range w[1:len(w)-1]{	//check station by station for overlap
+				for _, y := range w[1 : len(w)-1] { //check station by station for overlap
 
-					if x == y{
+					if x == y {
 
 						OverlapBool = true
 					}
@@ -72,14 +72,13 @@ for _, v := range (*found_routes) {
 
 			}
 
-			if OverlapBool == false{
+			if OverlapBool == false {
 				break
 			}
 
-
 		}
 
-		if OverlapBool == false{
+		if OverlapBool == false {
 
 			//two path group
 			fmt.Println("found non-overlap - OverlapBool = ", OverlapBool)
@@ -89,7 +88,7 @@ for _, v := range (*found_routes) {
 			PathGroupCombo = AddPathToGroup(PathGroupCombo, PathCandidate2)
 			fmt.Println(PathGroupCombo)
 			TopGroup.Groups = append(TopGroup.Groups, PathGroupCombo) //FIX!!!!!!!make TopGroup global address in main
-			
+
 			// if MaximumPaths > 2{
 			// 	CurrentPaths := 2
 			// 	PathCandidate1 = append(PathCandidate1[1:len(PathCandidate1)-1], PathCandidate2[1:len(PathCandidate2)-1]...)
@@ -110,60 +109,58 @@ for _, v := range (*found_routes) {
 			// }
 
 			break
-			}	
+		}
 
-} //FOR LOOP ENDS
+	} //FOR LOOP ENDS
 
-//counter := 1	
+	//counter := 1
 
-fmt.Println("returning THIS SHOULD HAPPEN IF THERE ARE MAX PATHS or LESS THAN MAX DUE TO ONLY OVERLAPPING PATHS, OverlapBool = ", OverlapBool)
-return
+	fmt.Println("returning THIS SHOULD HAPPEN IF THERE ARE MAX PATHS or LESS THAN MAX DUE TO ONLY OVERLAPPING PATHS, OverlapBool = ", OverlapBool)
+	return
 
 }
 
-func FindNonOverlappingPaths(Blacklist [][]string, CurrentPaths int, MaximumPaths int, found_routes *[][]string) bool, [][]string{
+/*
+func FindNonOverlappingPaths(Blacklist [][]string, CurrentPaths int, MaximumPaths int, found_routes *[][]string) (bool, [][]string) {
 
-NonOverlap := true
+	NonOverlap := true
 
-	for _, v := range (Blacklist) {
+	for _, v := range Blacklist {
 
 		NonOverlap = true
 
-		for j, w := range (*found_routes){
+		for j, w := range *found_routes {
 
-			for _, x := range v[1:len(v)-1]{ 	//choose paths to compare against each other
+			for _, x := range v[1 : len(v)-1] { //choose paths to compare against each other
 
-				for _, y := range w[1:len(w)-1]{	//check station by station for overlap
+				for _, y := range w[1 : len(w)-1] { //check station by station for overlap
 
-					if x == y{
+					if x == y {
 
 						NonOverlap = false
-				
+
 					}
 				}
 			}
 
 			//made it thru
 			if NonOverlap {
-			
+
 				copy_of_route := make([]string, len((*found_routes)[j]))
 				copy(copy_of_route, (*found_routes)[j])
 				Blacklist = append(Blacklist, copy_of_route)
-				
-				if CurrentPaths < MaximumPaths{
-				found, finalBlacklist := FindNonOverlappingPaths(Blacklist, (CurrentPaths+1), MaximumPaths, (*found_routes)[(j+1):])
+
+				if CurrentPaths < MaximumPaths {
+					found, finalBlacklist := FindNonOverlappingPaths(Blacklist, (CurrentPaths + 1), MaximumPaths, (*found_routes)[(j+1):])
 				}
 			}
 		}
 	}
 
-return true
+	return true
 
 }
-
-
-
-
+*/
 func CompareBlacklistToPath(start, end *data.Station, path *[]string, shortest_route_len *int, best_route *[]string, found_routes *[][]string) {
 
 	// Ignore loops
@@ -200,7 +197,7 @@ func CompareBlacklistToPath(start, end *data.Station, path *[]string, shortest_r
 
 	// Search deeper
 	for _, nodeptr := range start.Connections {
-		FindPathDFS(nodeptr, end, path, shortest_route_len, best_route, found_routes)
+		CompareBlacklistToPath(nodeptr, end, path, shortest_route_len, best_route, found_routes)
 	}
 
 	// Pop last item out and return
@@ -211,30 +208,24 @@ func CompareBlacklistToPath(start, end *data.Station, path *[]string, shortest_r
 
 }
 
-
 func AddPathToGroup(pathgroup data.GroupedPaths, path []string) data.GroupedPaths {
 
-		pathgroup.AmountOfPaths++
+	pathgroup.AmountOfPaths++
 
-		if len(path)-1 > pathgroup.LongestPathInTurns{
-		pathgroup.LongestPathInTurns = len(path)-1
-		}
+	if len(path)-1 > pathgroup.LongestPathInTurns {
+		pathgroup.LongestPathInTurns = len(path) - 1
+	}
 
-		copy_of_route := make([]string, len(path))
-		copy(copy_of_route, path)
+	copy_of_route := make([]string, len(path))
+	copy(copy_of_route, path)
 
-		pathgroup.Paths = append(pathgroup.Paths, copy_of_route)
+	pathgroup.Paths = append(pathgroup.Paths, copy_of_route)
 
-		return pathgroup
+	return pathgroup
 
 }
 
-
-
-
-
-
-// // // Depth-First-Search 
+// // // Depth-First-Search
 // func AllPathsThenMaxPaths(start, end *Station, path *[]string, shortest_route_len *int, best_route *[]string, found_routes *[][]string) {
 
 // 	// Ignore loops
@@ -281,7 +272,6 @@ func AddPathToGroup(pathgroup data.GroupedPaths, path []string) data.GroupedPath
 // 	// return
 
 // }
-
 
 // func NewGroupInit() *data.GroupedPaths {
 
